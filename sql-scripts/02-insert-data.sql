d63e34ea306c405be0c39a7ee8e41f72d9940e22
Alekhya <166293220+alekhyaogiboyina@users.noreply.github.com>
2026-03-06 00:18:47 +0530

    Create 02-insert-data.sql

---
+++ sql-scripts/02-insert-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Employees (EmpID, EmpName, Department) VALUES
+(1, 'Alekhya', 'IT'),
+(2, 'Bob', 'Development'),
+(3, 'Ram', 'Testing'),
+(4, 'Rahul', 'IT Support'),
+(5, 'Priya', 'Testing'),
+(6, 'Arjun', 'DevOps'),
+(7, 'Meena', 'Product Management');
